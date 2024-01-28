3410de0845b1ea8e27fb4724bb6c186b93bcc629
alxmart <alxmartinez@gmail.com>
2024-01-28 15:34:50 -0300

    Adding tests and exercises using OOP.

---
+++ lib/src/main/java/com/luizafmartinez/lib/IndiceAssuntoXAulas.java
@@ -0,0 +1,36 @@
+package com.luizafmartinez.lib;
+
+public class IndiceAssuntoXAulas {
+
+    /*
+
+     Classes,
+     Atributos,
+     Métodos       => Jogador / RunJogador
+
+     Métodos,
+     Retorno,
+     Parâmetros    => Funcionario / RunFuncionario
+
+     Construtores,
+     Sobrecarga de construtor,
+     Sobrecarga de Método        => Usuario / RunUsuario
+
+     Encapsulamento     => ContaBancaria / RunConta / Cliente
+
+     Herança    =>  Animal /Cachorro / Passaro / Heranca
+
+     Polimorfismo => Animal /Cachorro / Passaro / Heranca
+
+     Construtor com Herança => Animal /Cachorro / Passaro / Heranca
+
+     Classe concreta vs abstrata => Animal /Cachorro / Passaro / Heranca
+
+     Interface =>  Interface
+
+     Métodos Abstratos
+       e Passagem de Parâmetros   => Interface
+
+    */
+
+}
